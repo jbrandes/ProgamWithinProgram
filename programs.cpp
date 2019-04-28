e5fd874d4ce654de5871205db11ad7e3ae214003
Jordan Brandes <jordanbrandes@yahoo.com>
2019-04-28 12:51:09 -0500

    Update programs.cpp

--- programs.cpp
+++ programs.cpp
@@ -2,17 +2,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
+#include <cstdlib>
+
 
 using namespace std;
 
 
 
-
 int main () {
+
+int input;
 
 cout << "Which program would you like to run?" << endl;
 cout << "Press 1 for Bank Teller" << endl;
 cout << "Press 2 for Choose Your Own Adventure" << endl;
+cin >> input;
+
+
+	if (input == 1) {
+
+	std::system("./Bank");
+
+
+	}
+	
+	else if (input == 2) {
+	
+	
+	std::system("./Choose");
+
+	
+	}
+
 
 return 0;
 
